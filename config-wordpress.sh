adbe9521c3f6fce1cc9f091d8a4e466446cee15b
noname <37553600+yhvhdio@users.noreply.github.com>
2021-03-20 14:31:08 -0300

    .

--- config-wordpress.sh
+++ config-wordpress.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# set logs folder
+logs_file_path=/var/www/logs
+mkdir $logs_file_path
+chgrp www-data $logs_file_path
+chmod 765 $logs_file_path # 7(Owner: full) - 6(Group: write, read) - 5(Public: execute, read)
+
 # config WordPress
 if ! $(wp core is-installed  --allow-root --path='/var/www/html'); then
    echo "=> WordPress is not configured yet, configuring WordPress ..."
